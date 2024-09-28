bf8c0735136ec2820cc9dfccf1a6b087de871c22
Oluwagbenga Jolayemi <oluwagbengajolayemi@gmail.com>
2024-09-28 21:47:38 +0100

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,14 @@ const app = express();
 // Middleware to parse JSON requests
 app.use(express.json());
 
+// Middleware to log all GET requests
+app.use((req, res, next) => {
+  if (req.method === 'GET') {
+    console.log(`GET request made to: ${req.url}`);
+  }
+  next();
+});
+
 // Protected route
 app.get('/check', (req, res) => {
     res.send('working');
@@ -13,7 +21,6 @@ app.get('/check', (req, res) => {
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', uptime: process.uptime() });
 });
-
 
 // Start the server
 const PORT = 9000;
